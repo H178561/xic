480a67000600b626f9f029c6e593b54a3a9d651c
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-06-11 11:32:49 +0200

    but in ordering

--- src/amplitude.jl
+++ src/amplitude.jl
@@ -1,11 +1,13 @@
 function Lc2ppiKModel(; chains, couplings, isobarnames)
+    v = collect(enumerate(isobarnames))
+    # parse K2(number) or K(number), or L(number), or anyletter(number)
+    regex = r"[A-Za-z\d]+\((\d+)\)"
+    sort!(v, by=x -> eval(Meta.parse(match(regex, x[2])[1])))
+    # 
+    sort!(v, by=x -> findfirst(x[2][1], "LDK"))
+    order = getindex.(v, 1)
 
-	v = collect(enumerate(isobarnames))
-	sort!(v, by = x -> eval(Meta.parse(x[2][3:end-1])))
-	sort!(v, by = x -> findfirst(x[2][1], "LDK"))
-	order = getindex.(v, 1)
-
-	mypairs = isobarnames[order] .=> zip(couplings[order], chains[order])
-	x = Vector{Pair{String, Tuple{Complex, AbstractDecayChain}}}(mypairs)
-	ThreeBodyDecay(x)
+    mypairs = isobarnames[order] .=> zip(couplings[order], chains[order])
+    x = Vector{Pair{String,Tuple{Complex,AbstractDecayChain}}}(mypairs)
+    ThreeBodyDecay(x)
 end
